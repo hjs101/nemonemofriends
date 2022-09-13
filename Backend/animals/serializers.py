8b9269db198ae1209343bde06bd6a6c392fd350b
노은영 <ndh0210@naver.com>
2022-09-13 11:49:55 +0900

    #5 ✨ 도감 이름 변경 시리얼라이저 추가

--- Backend/animals/serializers.py
+++ Backend/animals/serializers.py
@@ -1,5 +1,14 @@
+from dataclasses import fields
 from . import models
 from rest_framework import serializers
+
+
+class AnimalsRenameSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.User_Animal
+        fields = ('name',)
+
 
 class AnimalsTestSerializer(serializers.ModelSerializer):
     
