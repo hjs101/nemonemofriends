5e955ed74613ed2b8c7eced4521bafefb20700ac
노은영 <ndh0210@naver.com>
2022-09-13 11:51:03 +0900

    🎨 홑따옴표 통일

--- Backend/project/settings.py
+++ Backend/project/settings.py
@@ -120,14 +120,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES={
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': env('DB_NAME'),
-'USER': env('DB_USER'),
-'PASSWORD': env('DB_PASSWORD'),
-"HOST": env('DB_HOST'),
-"PORT": env('DB_PORT'),
-}
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
 }
 
 
