ab011856a4531e6f4b20bf3eb97e9ebdbfa31a15
노은영 <ndh0210@naver.com>
2022-09-13 23:13:28 +0900

    #4 ✨ item앱 관리자 생성

--- Backend/items/admin.py
+++ Backend/items/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Decoration, User_Decoration, Color, User_Color
 
-# Register your models here.
+
+class DecorationAdmin(admin.ModelAdmin):
+    fields = ['cost', 'is_rare']
+
+admin.site.register(Decoration, DecorationAdmin)
+
+
+class UserDecorationAdmin(admin.ModelAdmin):
+    fields = ['user', 'decoration', 'is_located', 'location', 'angle']
+
+admin.site.register(User_Decoration, UserDecorationAdmin)
+
+
+class ColorAdmin(admin.ModelAdmin):
+    readonly_fields  = ['id']
+
+admin.site.register(Color, ColorAdmin) 
+
+
+class UserColorAdmin(admin.ModelAdmin):
+    fields = ['user', 'color', 'cnt']
+
+admin.site.register(User_Color, UserColorAdmin)
+
+
