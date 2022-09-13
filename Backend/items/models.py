f8665359dbc087a4bc540806073bb4a35a0dae44
조예지 <celpegor216@gmail.com>
2022-09-13 11:36:34 +0900

    #2 🐛 유저 정보 확인 추가

--- Backend/items/models.py
+++ Backend/items/models.py
@@ -9,7 +9,7 @@ class Decoration(models.Model):
 
 class User_Decoration(models.Model):
     id = models.AutoField(primary_key=True)
-    # user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     decoration_id = models.ForeignKey('Decoration', on_delete=models.CASCADE)
     is_located = models.BooleanField()
     location = models.IntegerField(blank=True, null=True)
@@ -22,6 +22,6 @@ class Color(models.Model):
 
 class User_Color(models.Model):
     id = models.AutoField(primary_key=True)
-    # user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     color_id = models.ForeignKey('Color', on_delete=models.CASCADE)
     cnt = models.IntegerField(default=1)
