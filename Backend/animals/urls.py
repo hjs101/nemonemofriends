5cd0d7b7028006774cf393ff4902af0cf4cc1f20
노은영 <ndh0210@naver.com>
2022-09-15 23:51:36 +0900

    #5 ✅ depth 테스트

--- Backend/animals/urls.py
+++ Backend/animals/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('eat/', views.AnimalsEatView.as_view()),
     path('color/', views.AnimalsColorView.as_view()),
     path('rename/', views.AnimalsRenameView.as_view()),
-    # path('test/<int:animal_id>/<int:order_id>/', views.AnimalsTestView.as_view()),
     path('talk/', views.AnimalsTalkView.as_view()),
+    path('depthtest/<int:id>/', views.DepthTestView.as_view()),
+    # path('test/<int:animal_id>/<int:order_id>/', views.AnimalsTestView.as_view()),
     # path('play/wordchain/', views.AnimalsPlayWordChainView.as_view()),
     # path('play/newgame/', views.AnimalsPlayNewGame.as_view()),
 ]
