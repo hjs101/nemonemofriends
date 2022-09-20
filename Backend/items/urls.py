4604aa8d6343f197f42dd43f7308b19037674fd3
조예지 <celpegor216@gmail.com>
2022-09-20 18:03:07 +0900

    #2 ✨ 조경 배치, 해제

--- Backend/items/urls.py
+++ Backend/items/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.ItemsCreateView.as_view(), name='create'),
-    path('update/', views.ItemsUpdateView.as_view(), name='update'),
     path('buy/', views.ItemsBuyView.as_view(), name='buy'),
+    path('place/', views.ItemsPlaceView.as_view(), name='place'),
+    path('update/', views.ItemsUpdateView.as_view(), name='update'),
+    path('cancel/', views.ItemsCancelView.as_view(), name='cancel'),
 ]
