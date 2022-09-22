d25154a9818d42e4c76c54eda727127dab9acd04
노은영 <ndh0210@naver.com>
2022-09-22 10:46:41 +0900

    #4 ✨ 대화 - 전체 명령

--- Backend/animals/views.py
+++ Backend/animals/views.py
@@ -7,9 +7,10 @@ from rest_framework.response import Response
 
 from .models import Animal, User_Animal
 from .serializers import AnimalsRenameSerializer, AnimalsTestSerializer, UserAnimalSerializer
+from .utils import *
 from animals import serializers
 from items.models import User_Item
-from utils import *
+from utils import SUCCESS, FAIL
 
 import random
 from datetime import datetime, timedelta
@@ -112,23 +113,27 @@ class AnimalsColorView(APIView):
         response["message"] = "유저 정보가 다릅니다."
         return Response(response)
 
-ALL_COMMANDS = ['얘들아', '모두']
-
-def speech_to_text(data=None):
-    if not data:
-        data = "노란아 앉아"
-    return data
 
 class AnimalsTalkView(APIView):
-    # 전체 명령
-    def talk_to_all():
-        pass
+    # 전체 동물 대화
+    def talk_to_all(context, user):
+        for command in ALL_COMMANDS:
+            if command in context:
+                if not user.is_called:
+                    user = reward_gold(user, 'talking_all')
+                    user.is_called = True
+                    user.save()
+                response = {'command': command}
+                response.update(SUCCESS)
 
-    # 특정 동물
+                return response
+        return FAIL
+
+    # 특정 동물 대화
     def talk_to_one(animal, user, context):
         action = 'talking_one'
-        level = animal.level
-        commands = animal.commands[:level+1]
+        grade = animal.grade
+        commands = animal.commands[:grade+1]
 
         for command in commands:
             if command in context:
@@ -146,7 +151,7 @@ class AnimalsTalkView(APIView):
 
         return FAIL
 
-    def put(self, request):
+    def post(self, request):
         audio = request.data.get('voice')
         context = speech_to_text(audio)
         # print(context)
@@ -159,61 +164,16 @@ class AnimalsTalkView(APIView):
                 response = self.talk_to_one(animal, user, context)
                 return Response(response)
         
-        response = self.talk_to_all(context)
+        response = self.talk_to_all(context, user)
         return Response(response)
 
-        # print(request.__dir__())
-        # print(request.parsers)
-        # print(request.parser_context)
-        # user_animal_id = request.data.get('id')
-        # print(user_animal_id)
-        # user_animal = get_object_or_404(User_Animal, pk=user_animal_id)
-        # print(request.user)
-        # if request.user == user_animal.user:
-        #     result = self.recognize(request.data.get('voice'))
-        #     level = user_animal.level
-        #     commands = user_animal.animal.commands[:level+1]
-        #     print(request.user.animal_set.all())
-        #     if result in commands:
-        #         action = 'talking'
-        #         user_animal.playing_cnt += 1
-        #         user_animal = reward_exp(user_animal, request.user, action)
-        #         user_animal.save()
-        #         user = reward_gold(request.user, action)
-        #         user.save()
-        #         return Response(SUCCESS)
 
-
-
-
-# class AnimalsPlayNewGame(APIView):
-#     def put(self, request):
-#         pass
+class AnimalPlayMaze(APIView):
+    def put(self, request):
+        pass
 
 
 class AnimalsPlayWordChainView(APIView):
     def put(self, request):
         pass
 
-
-# class AnimalsTestView(APIView):
-#     def get(self, request, animal_id, order_id):
-#         print(request)
-#         print(order_id, type(order_id))
-#         animals = get_list_or_404(Animal)
-#         animal = get_object_or_404(Animal, id=animal_id)
-#         print(animals)
-#         # serializer = AnimalsTestSerializer(animals, many=True)
-#         serializer = AnimalsTestSerializer(animal)
-#         print(animal)
-#         space = animal.species
-#         feed = animal.feeds
-#         character = animal.characteristics
-#         commands = animal.commands
-#         print(f'space: {space}\ncharacter: {character}\ncommands: {commands}\nfeed: {feed}')
-#         # print('명령', commands[str(order_id)])
-#         print(commands.keys())
-#         print('먹이', type(feed[order_id]), feed[order_id])
-#         return Response(serializer.data)
-
-print('view 내부')
--- Backend/utils.py
+++ Backend/utils.py
@@ -3,32 +3,5 @@ SUCCESS = {"success":True}
 FAIL = {"success":False}
 
 
-# date 포맷 형식
-date_format_slash = f'%y/%m/%d/%H/%M/%S'
 
 
-# gold 보상 처리
-def reward_gold(user, action):
-    reward = {'eatting': 100, 'level_up': 777, 'talking_one': 100, }
-    user.gold += reward[action]
-    return user
-
-
-# exp 보상 처리
-def reward_exp(animal, user, action):
-    lookup_grade = [1, 1, 1, 2, 2, 3]  # lookup_grade[level] = grade
-    levelup_exp = [0, 0, 100, 200, 300, 400, float('inf')]
-    reward = {'eatting': 80, 'talking_one': 50}
-
-    exp = animal.exp + reward[action]
-    next_level = animal.level + 1
-
-    if levelup_exp[next_level] <= exp:
-        user = reward_gold(user, 'level_up')
-        user.save()
-        exp -= levelup_exp[next_level]
-        animal.level = next_level
-        animal.grade = lookup_grade[next_level]
-
-    animal.exp = exp
-    return animal
