7154b6c1d6e9a4dee0cef2c6e53fba51bbc9f40b
노은영 <ndh0210@naver.com>
2022-09-25 01:45:10 +0900

    #5 ✨ 미로 / 경험치 물약 api

--- Backend/animals/urls.py
+++ Backend/animals/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('rename/', views.AnimalsRenameView.as_view()),
     path('talk/', views.AnimalsTalkView.as_view()),
     path('place/', views.AnimalsPlaceView.as_view()),
+    path('expup/', views.AnimalsExpUpView.as_view()),
+    path('play/maze/', views.AnimalsMazeView.as_view()),
     path('play/wordchain/start/', views.AnimalsPlayWordchainStartView.as_view()),
     path('play/wordchain/next/', views.AnimalsPlayWordchainNextView.as_view()),
     path('play/wordchain/finish/', views.AnimalsPlayWordchainFinishView.as_view()),
