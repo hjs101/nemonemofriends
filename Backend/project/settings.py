289fc8291892308d725cc94721a8c4544cef5650
h5282001 <h5282001@naver.com>
2022-09-28 17:15:41 +0900

    #5 도커 Redis 테스트

--- Backend/project/settings.py
+++ Backend/project/settings.py
@@ -288,7 +288,7 @@ LOGGING = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1", # 1번 DB
+        "LOCATION": "redis://some-redis:6379/1", # 1번 DB
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
