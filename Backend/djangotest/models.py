9d18751229571d1e943bdd69b0bb3ad7a3272eea
노은영 <ndh0210@naver.com>
2022-10-06 15:19:26 +0900

    #5 🔥 djangotest 앱 삭제
    Former-commit-id: 546e1dce6d4567c7d70d8eeead34b057135bef44

--- Backend/djangotest/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Test(models.Model):
-    id = models.CharField(max_length=64, primary_key=True)
-    success = models.CharField(max_length=20, default="Success!!")
-
-class ArrayTest(models.Model):
-    column = models.JSONField(default=dict)
